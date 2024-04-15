5ee7d49544e1dd00cce17048cae7f6e7b0557a13
IvelinEvIvanov <ivelin.ev.ivanov@gmail.com>
2024-04-15 17:52:03 +0300

    updated project

---
+++ _01_JavaFundamentals/src/_03_arrays/lab/_04_ReverseAnArrayOfStrings.java
@@ -0,0 +1,27 @@
+package _03_arrays.lab;
+
+import java.util.Scanner;
+
+public class _04_ReverseAnArrayOfStrings {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		String[] charArr = scanner.nextLine().split(" ");
+
+		int left = 0;
+		int right = charArr.length - 1;
+
+		while (left < right) {
+			String temp = charArr[left];
+			charArr[left++] = charArr[right];
+			charArr[right--] = temp;
+		}
+
+		scanner.close();
+
+		String reversedArr = String.join(" ", charArr);
+		System.out.println(reversedArr);
+	}
+
+}
