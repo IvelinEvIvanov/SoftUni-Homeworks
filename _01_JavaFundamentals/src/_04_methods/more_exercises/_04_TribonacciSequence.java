4fd2d69a80d78edf2f8eed62f17829c0e107769c
IvelinEvIvanov <ivelin.ev.ivanov@gmail.com>
2024-04-21 16:11:09 +0300

    added tasks

---
+++ _01_JavaFundamentals/src/_04_methods/more_exercises/_04_TribonacciSequence.java
@@ -0,0 +1,38 @@
+package _04_methods.more_exercises;
+
+import java.util.Scanner;
+
+public class _04_TribonacciSequence {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		scanner.close();
+
+		int[] triboArr = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			triboArr[i] = getTriboSum(triboArr, i);
+			System.out.print(triboArr[i] + " ");
+		}
+	}
+
+	private static int getTriboSum(int[] triboArr, int i) {
+		switch (i) {
+		case 0:
+			return 1;
+		case 1:
+			return 1;
+		case 2:
+			return 2;
+		default: {
+
+			int sum = 0;
+			for (int j = i - 1; j >= i - 3; j--) {
+				sum += triboArr[j];
+			}
+			return sum;
+		}
+		}
+	}
+}
---
+++ _01_JavaFundamentals/src/_04_methods/more_exercises/_05_MultiplicationSign.java
@@ -0,0 +1,51 @@
+package _04_methods.more_exercises;
+
+import java.util.Scanner;
+
+public class _05_MultiplicationSign {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int[] numArr = new int[3];
+
+		numArr[0] = scanner.nextInt();
+		numArr[1] = scanner.nextInt();
+		numArr[2] = scanner.nextInt();
+		scanner.close();
+
+		boolean hasZero = hasAtLeastOneZero(numArr);
+		
+		if (hasZero) {
+			System.out.println("zero");
+			return;
+		}
+
+		int result = countNegativeNumbers(numArr);
+		
+		if (result % 2 == 0) {
+			System.out.println("positive");
+		} else {
+			System.out.println("negative");
+		}
+
+	}
+
+	private static boolean hasAtLeastOneZero(int[] numArr) {
+		for (int i = 0; i < numArr.length; i++) {
+			if (numArr[i] == 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static int countNegativeNumbers(int[] numArr) {
+		int count = 0;
+		for (int i = 0; i < numArr.length; i++) {
+			if (numArr[i] < 0) {
+				count++;
+			}
+		}
+		return count;
+	}
+}
