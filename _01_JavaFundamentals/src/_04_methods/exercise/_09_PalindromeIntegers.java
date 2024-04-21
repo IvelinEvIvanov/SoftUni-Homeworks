5adaee928466c200b58169d9a5350aa4d024dd14
IvelinEvIvanov <ivelin.ev.ivanov@gmail.com>
2024-04-21 10:09:56 +0300

    added tasks

---
+++ _01_JavaFundamentals/src/_04_methods/exercise/_09_PalindromeIntegers.java
@@ -0,0 +1,36 @@
+package _04_methods.exercise;
+
+import java.util.Scanner;
+
+public class _09_PalindromeIntegers {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		while (true) {
+			String num = scanner.nextLine();
+
+			if (num.equals("END")) {
+				break;
+			}
+
+			String reversedNum = reverseNum(num);
+
+			if (reversedNum.equals(num)) {
+				System.out.println(true);
+			} else {
+				System.out.println(false);
+			}
+		}
+
+		scanner.close();
+	}
+
+	private static String reverseNum(String num) {
+		String reversedNum = "";
+		for (int i = num.length() - 1; i >= 0; i--) {
+			reversedNum += num.charAt(i);
+		}
+		return reversedNum;
+	}
+}
