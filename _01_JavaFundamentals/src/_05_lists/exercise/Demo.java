60a566a36907453ba4a71fd13503175d4fdbf6d7
IvelinEvIvanov <ivelin.ev.ivanov@gmail.com>
2024-04-22 13:00:28 +0300

    added tasks

---
+++ _01_JavaFundamentals/src/_05_lists/exercise/Demo.java
@@ -0,0 +1,19 @@
+package _05_lists.exercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		List<Integer> nums = Arrays	.stream(scanner	.nextLine()
+													.split(" "))
+									.map(Integer::parseInt)
+									.collect(Collectors.toList());
+		scanner.close();
+	}
+
+}
